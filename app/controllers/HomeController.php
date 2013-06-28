ce273367419279bb4bfbc2b7dd1dfd4cef14a027
Sander <s@nder.be>
2013-06-28 10:47:16 +0200

    Cleanup home for faster rendering.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -13,122 +13,142 @@ class HomeController extends BaseController {
   }
 
   public function getHome() {
-    
-
-
-
-    $max = 0;
-    $min = 1000000;
-
-    $accountsKey = cacheKey('getHome', 'accounts');
-    $budgetsKey  = cacheKey('getHome', 'budgets');
-    $targetKey = cacheKey('getHome','targets');
-    Cache::has($accountsKey);
-    if (Cache::has($accountsKey)) {
-      $accounts = Cache::get($accountsKey);
-
+    $key = cacheKey('home', Session::get('period'));
+    if (Cache::has($key)) {
+      $data = Cache::get($key);
     } else {
+      $max  = 0;
+      $min  = 1000000;
+      $data = array(
+          'accounts' => array(),
+          'budgets'  => array(),
+          'targets'  => array()
+      );
+
       $accounts = Auth::user()->accounts()->get();
+      foreach ($accounts as $a) {
+        $account           = array(
+            'id'             => intval($a->id),
+            'name'           => Crypt::decrypt($a->name),
+            'currentbalance' => $a->balance()
+        );
+        $account['header'] = $account['currentbalance'] < 0 ? array('style' => 'color:red;', 'class' => 'tt', 'title' => $account['name'] . ' has a balance below zero. Try to fix this.') : array();
 
-      foreach ($accounts as $account) {
-        $account->name           = Crypt::decrypt($account->name);
-        $account->currentbalance = $account->balance();
-        $min                     = $account->currentbalance < $min ? $account->currentbalance : $min;
-        $max                     = $account->currentbalance > $max ? $account->currentbalance : $max;
+        $min = $account['currentbalance'] < $min ? $account['currentbalance'] : $min;
+        $max = $account['currentbalance'] > $max ? $account['currentbalance'] : $max;
 
-        // get last five transactions and transfers.
+// last transactions and transfers for this account:
         $list = array();
-        foreach ($account->transactions()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
-          $date          = $t->date;
-          $list[$date]   = isset($list[$date]) ? $list[$date] : array();
-          $list[$date][] = $t;
+        foreach ($a->transactions()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
+          $date             = $t->date;
+          $list[$date]      = isset($list[$date]) ? $list[$date] : array();
+          $t->description   = Crypt::decrypt($t->description);
+          $t->date          = new DateTime($t->date);
+          $t->category_name = is_null($t->category_id) ? null : Crypt::decrypt($t->category()->first()->name);
+          $t->type          = 'Transaction';
+          $list[$date][]    = $t->toArray();
         }
-        foreach ($account->transfersto()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
-          $date          = $t->date;
-          $list[$date]   = isset($list[$date]) ? $list[$date] : array();
-          $list[$date][] = $t;
+        foreach ($a->transfersto()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
+          $date                 = $t->date;
+          $list[$date]          = isset($list[$date]) ? $list[$date] : array();
+          $t->date              = new DateTime($t->date);
+          $t->description       = Crypt::decrypt($t->description);
+          $t->category_name     = is_null($t->category_id) ? null : Crypt::decrypt($t->category()->first()->name);
+          $t->account_to_name   = Crypt::decrypt($t->accountto()->first()->name);
+          $t->account_from_name = Crypt::decrypt($t->accountfrom()->first()->name);
+          $t->type              = 'Transfer';
+          $list[$date][]        = $t->toArray();
         }
-        foreach ($account->transfersfrom()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
-          $date          = $t->date;
-          $list[$date]   = isset($list[$date]) ? $list[$date] : array();
-          $list[$date][] = $t;
+        foreach ($a->transfersfrom()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
+          $date                 = $t->date;
+          $list[$date]          = isset($list[$date]) ? $list[$date] : array();
+          $t->description       = Crypt::decrypt($t->description);
+          $t->date              = new DateTime($t->date);
+          $t->category_name     = is_null($t->category_id) ? null : Crypt::decrypt($t->category()->first()->name);
+          $t->account_to_name   = Crypt::decrypt($t->accountto()->first()->name);
+          $t->account_from_name = Crypt::decrypt($t->accountfrom()->first()->name);
+          $t->type              = 'Transfer';
+          $list[$date][]        = $t->toArray();
         }
         krsort($list);
-        $account->list = $list;
+        $account['list']    = $list;
+        $data['accounts'][] = $account;
       }
-      // order the list.
-
-
 
       $min = $min > 0 ? 0 : $min;
       $max = $max < 0 ? 0 : $max;
-      // we make steps of a 1000 euro's.
       $min = floor($min / 1000) * 1000;
       $max = ceil($max / 1000) * 1000;
-      foreach ($accounts as $account) {
-        $maxpct          = $max != 0 ? ceil(($account->currentbalance / $max) * 100) : 0;
-        $account->maxpct = $maxpct < 0 ? 0 : $maxpct;
-        $minpct          = $min != 0 ? floor(($account->currentbalance / $min) * 100) : 0;
-        $account->minpct = $minpct < 0 ? 0 : $minpct;
+      $sum = 0;
+      foreach ($data['accounts'] as $index => $account) {
+        $maxpct                             = $max != 0 ? ceil(($account['currentbalance'] / $max) * 100) : 0;
+        $data['accounts'][$index]['maxpct'] = $maxpct < 0 ? 0 : $maxpct;
+        $minpct                             = $min != 0 ? floor(($account['currentbalance'] / $min) * 100) : 0;
+        $data['accounts'][$index]['minpct'] = $minpct < 0 ? 0 : $minpct;
+        $sum+= $account['currentbalance'];
       }
-      Cache::put($accountsKey, $accounts, 1440);
-    }
+      $data['acc_data']['sum'] = $sum;
 
-    // budgets:
-    if (Cache::has($budgetsKey)) {
-      $budgets = Cache::get($budgetsKey);
-    } else {
+
+// now everything for budgets:
       $budgets = Auth::user()->budgets()->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->get();
-      foreach ($budgets as $budget) {
-        $budget->name     = Crypt::decrypt($budget->name);
-        $budget->widthpct = $budget->amount > 0 ? ceil(($budget->spent() / $budget->amount) * 100) : 100;
-        $budget->expected = $budget->expected();
-        $budget->overflow = $budget->expected > $budget->left();
-        $budget->amount = floatval($budget->amount);
-        // get last five transactions and transfers:
-        $list = array();
-        foreach ($budget->transactions()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
-          $date          = $t->date;
-          $list[$date]   = isset($list[$date]) ? $list[$date] : array();
-          $list[$date][] = $t;
-        }
-        foreach ($budget->transfers()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
-          $date          = $t->date;
-          $list[$date]   = isset($list[$date]) ? $list[$date] : array();
-          $list[$date][] = $t;
-        }
-        $budget->list = $list;
-      }
-      Cache::put($budgetsKey,$budgets,1440);
-    }
-
-    // targets
-    if(!Cache::has($targetKey) && count($accounts) > 1) {
-      $targets = array();
-      $db = Auth::user()->targets()->orderBy('duedate','DESC')->get();
-      foreach($db as $t) {
-        $tr = array(
-            'id' => $t->id,
-            'description' => Crypt::decrypt($t->description),
-            'amount' => floatval($t->amount),
-            'duedate' => $t->duedate != '0000-00-00' ? new DateTime($t->duedate) : null,
-            'startdate' => $t->startdate != '0000-00-00' ? new DateTime($t->startdate) : null,
-            'saved' => $t->hassaved(),
-            'guide' => $t->guide(),
-            'should' => $t->shouldhavesaved()
+      foreach ($budgets as $b) {
+        $budget             = array(
+            'id'       => intval($b->id),
+            'name'     => Crypt::decrypt($b->name),
+            'widthpct' => $b->amount > 0 ? ceil(($b->spent() / $b->amount) * 100) : 100,
+            'expected' => $b->expected(),
+            'spent'    => $b->spent(),
+            'left'     => $b->left(),
+            'advice'   => $b->advice(),
+            'amount'   => floatval($b->amount),
         );
-        $tr['pct'] = round(($tr['saved'] / $tr['amount']) * 100,2);
-        $targets[] = $tr;
+        $budget['overflow'] = $budget['expected'] > $b->left();
+
+        $list = array();
+        foreach ($b->transactions()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
+          $date             = $t->date;
+          $t->description   = Crypt::decrypt($t->description);
+          $list[$date]      = isset($list[$date]) ? $list[$date] : array();
+          $t->date          = new DateTime($t->date);
+          $t->type          = 'Transaction';
+          $t->category_name = is_null($t->category_id) ? null : Crypt::decrypt($t->category()->first()->name);
+          $list[$date][]    = $t->toArray();
+        }
+        foreach ($b->transfers()->take(5)->where(DB::Raw('DATE_FORMAT(`date`,"%m-%Y")'), '=', Session::get('period')->format('m-Y'))->orderBy('date', 'DESC')->orderBy('created_at', 'DESC')->get() as $t) {
+          $date                 = $t->date;
+          $t->description       = Crypt::decrypt($t->description);
+          $list[$date]          = isset($list[$date]) ? $list[$date] : array();
+          $t->date              = new DateTime($t->date);
+          $t->type              = 'Transfer';
+          $t->account_to_name   = Crypt::decrypt($t->accountto()->first()->name);
+          $t->account_from_name = Crypt::decrypt($t->accountfrom()->first()->name);
+          $t->category_name     = is_null($t->category_id) ? null : Crypt::decrypt($t->category()->first()->name);
+          $list[$date][]        = $t->toArray();
+        }
+        $budget['list']    = $list;
+        $data['budgets'][] = $budget;
       }
 
-      Cache::put($targetKey,$targets,1440);
-    } else {
-      $targets = Cache::get($targetKey);
+      // targets
+      $db = Auth::user()->targets()->orderBy('duedate', 'DESC')->get();
+      foreach ($db as $t) {
+        $tr                = array(
+            'id'          => $t->id,
+            'description' => Crypt::decrypt($t->description),
+            'amount'      => floatval($t->amount),
+            'duedate'     => $t->duedate != '0000-00-00' ? new DateTime($t->duedate) : null,
+            'startdate'   => $t->startdate != '0000-00-00' ? new DateTime($t->startdate) : null,
+            'saved'       => $t->hassaved(),
+            'guide'       => $t->guide(),
+            'should'      => $t->shouldhavesaved()
+        );
+        $tr['pct']         = round(($tr['saved'] / $tr['amount']) * 100, 2);
+        $data['targets'][] = $tr;
+      }
+      Cache::put($key, $data, 2440);
     }
-
-
-    return View::make('home.home')->with('accounts', $accounts)->with('budgets', $budgets)
-            ->with('targets',$targets);
+    return View::make('home.home')->with('data', $data);
   }
 
   public function getIndex() {
@@ -144,6 +164,7 @@ class HomeController extends BaseController {
   public function askDelete() {
     return View::make('home.delete');
   }
+
   public function doDelete() {
     $user = Auth::user();
     Auth::logout();
@@ -151,7 +172,6 @@ class HomeController extends BaseController {
     $user->delete();
     return Redirect::to(UserService::createLogoutUrl('/'));
   }
-
 
   public function showConcept() {
     return View::make('home.concept');
