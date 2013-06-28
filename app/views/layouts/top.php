9fe5e816d47d9b24606d5da54c25001d19d74cf9
Sander <s@nder.be>
2013-06-28 13:51:36 +0200

    Add warnings and other session things.

--- app/views/layouts/top.php
+++ app/views/layouts/top.php
@@ -10,4 +10,38 @@
   </head>
   <body>
     <?php require_once(__DIR__.'/../snippets/menu.php'); ?>
+
+    <?php if(Session::has('error')): ?>
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="alert alert-error">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Error:</strong> <?php echo Session::get('error');?>
+          </div>
+        </div>
+      </div>
+      <?php endif; ?>
+      <?php if(Session::has('success')): ?>
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="alert alert-success">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>OK</strong> <?php echo Session::get('success'); ?>
+          </div>
+        </div>
+      </div>
+      <?php endif; ?>
+
+    <?php if(Session::has('warning')): ?>
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="alert alert-warning">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Warning</strong> <?php echo Session::get('warning'); ?>
+          </div>
+        </div>
+      </div>
+      <?php endif; ?>
+
+
     <div class="container-fluid">
