5afed571d7b3365cff25589bee33678da3c0cc60
Sander <s@nder.be>
2013-06-29 08:48:56 +0200

    Renamed some files to avoid Blade problems.

--- app/views/transfers/all.php
+++ app/views/transfers/all.php
@@ -2,64 +2,37 @@
 
 <div class="row-fluid">
   <div class="span12">
+    <h2>All transactions you ever made!</h2>
 
-    <table class="table">
-      <?php foreach ($transfers as $t) { ?>
+
+      <?php foreach ($transactions as $period => $tr): ?>
+        <h3><?php echo $period;?></h3>
+        <table class="table table-bordered table-condensed table-striped">
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Account</th>
+            <th>Budget</th>
+            <th>Category</th>
+            <th>Beneficiary</th>
+            <th>&nbsp;</th>
+          </tr>
+        <?php foreach($tr as $t):?>
         <tr>
-          <td><?php echo $t->date; ?></td>
-          <td><?php echo Crypt::decrypt($t->description); ?></td>
-          <td><?php echo $t->amount; ?></td>
-          <td>
-            <?php
-            if ($t->account_from != null) {
-              echo Crypt::decrypt($t->accountfrom()->first()->name);
-            }
-            ?>
-            &rarr;
-            <?php
-            if ($t->account_to != null) {
-              echo Crypt::decrypt($t->accountto()->first()->name);
-            }
-            ?>
-          </td>
-          <td>
-            <?php
-            if ($t->budget_id != null) {
-              echo Crypt::decrypt($t->budget()->first()->name);
-            }
-            ?>
-          </td>
-          <td>
-            <?php
-            if ($t->beneficiary_id != null) {
-              echo Crypt::decrypt($t->beneficiary()->first()->name);
-            }
-            ?>
-          </td>
-          <td>
-            <?php
-            if ($t->category_id != null) {
-              echo Crypt::decrypt($t->category()->first()->name);
-            }
-            ?>
-          </td>
-
-          <td>
-            <?php
-            if ($t->target_id != null) {
-              echo Crypt::decrypt($t->target()->first()->description);
-            }
-            ?>
-          </td>
-          <td>Ignore: <?php echo ($t->ignorepredicition == 1 ? 1 : 0); ?></td>
-          <td>Expense: <?php echo ($t->countasexpense == 1 ? 1 : 0); ?></td>
-
-
-
+          <td><?php echo $t['date']; ?></td>
+          <td><?php echo HTML::Link('/home/transaction/edit/'.$t['id'],$t['description']); ?></td>
+          <td><?php echo $t['amount'] ?></td>
+          <td><?php echo HTML::Link('/home/account/overview/' . $t['account_id'],$t['account_name']);?></td>
+          <td><?php echo !is_null($t['budget_id']) ? HTML::Link('/home/budget/overview/' . $t['budget_id'],$t['budget_name']) : '';?></td>
+          <td><?php echo !is_null($t['category_id']) ? HTML::Link('/home/category/overview/' . $t['category_id'],$t['category_name']) : '';?></td>
+          <td><?php echo !is_null($t['beneficiary_id']) ? HTML::Link('/home/beneficiary/overview/' . $t['beneficiary_id'],$t['beneficiary_name']) : '';?></td>
+          <td><a href="/home/transaction/delete/<?php echo $t['id']; ?>" title="Delete <?php echo $t['description'];?>" class="btn btn-danger"><i class="icon-white icon-remove"></i></a>
         </tr>
+        <?php endforeach;?>
+</table>
+<?php endforeach;?>
 
-      <?php } ?>
-    </table>
   </div>
 </div>
 
---
+++ app/views/transfers/edit.php
@@ -0,0 +1,125 @@
+<?php require_once(__DIR__ . '/../layouts/top.php') ?>
+<div class="row-fluid">
+  <div class="span6">
+    <h3>Add a new transfer</h3>
+    <p>
+      Please fill in the following details about the new transfer.
+    </p>
+
+    <?php echo Form::open(array('class' => 'form-horizontal')); ?>
+    <div class="control-group">
+      <label class="control-label" for="inputDescription">New transfer description</label>
+      <div class="controls">
+        <?php echo Form::text('description',null,array('id' => 'inputDescription','class' => 'input-xxlarge','autocomplete' => 'off','placeholder' => 'Moved some money to saving account')); ?>
+        <br /><span class="text-error"><?php echo $errors->first('name'); ?></span>
+      </div>
+    </div>
+    <div class="control-group">
+      <label class="control-label" for="inputBalance">Transferred amount</label>
+      <div class="controls">
+        <?php echo Form::input('number', 'amount',null,array('step' => 'any','autocomplete' => 'off', 'id' => 'inputAmount','placeholder' => '&euro;')); ?>
+        <br /><span class="text-error"><?php echo $errors->first('amount'); ?></span>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="inputDate">Transfer date</label>
+      <div class="controls">
+        <?php echo Form::input('date', 'date',date('Y-m-d'),array('id' => 'inputDate','autocomplete' => 'off')); ?>
+        <br /><span class="text-error"><?php echo $errors->first('date'); ?></span>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="inputAccount">From account</label>
+      <div class="controls">
+        <?php echo Form::select('account_from',$accounts); ?>
+        <br /><span class="text-error"><?php echo $errors->first('account_from'); ?></span>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="inputAccount">To account</label>
+      <div class="controls">
+        <?php echo Form::select('account_to',$accounts); ?>
+        <br /><span class="text-error"><?php echo $errors->first('account_to'); ?></span>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="inputBudget">Falls within budget</label>
+      <div class="controls">
+        <?php echo Form::select('budget',$budgets); ?>
+        <br /><span class="text-error"><?php echo $errors->first('budget_id'); ?></span>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="inputCategory">Category</label>
+      <div class="controls">
+        <?php echo Form::text('category',null,array('id' => 'inputCategory','autocomplete' => 'off','class' => 'input-large','placeholder' => 'Category','list' => 'addTargetCategory')); ?>
+        &nbsp;&nbsp;<img class="tt" title="This is a free field, which will suggest previous categories." src="/img/icons/help.png" alt="Help on this field" />
+        <br /><span class="text-error"><?php echo $errors->first('category_id'); ?></span>
+        <datalist id="addTargetCategory">
+          <?php foreach($categories as $cat): ?>
+          <option value="<?php echo $cat; ?>"></option>
+          <?php endforeach;?>
+        </datalist>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="inputTarget">Saving target</label>
+      <div class="controls">
+        <?php echo Form::text('target',null,array('id' => 'inputTarget','autocomplete' => 'off','class' => 'input-large','placeholder' => 'Target','list' => 'addTransferTarget')); ?>
+        &nbsp;&nbsp;<img class="tt" title="This is a free field, which will suggest previous categories." src="/img/icons/help.png" alt="Help on this field" />
+        <br /><span class="text-error"><?php echo $errors->first('target_id'); ?></span>
+        <datalist id="addTransferTarget">
+          <?php foreach($targets as $t): ?>
+          <option value="<?php echo $t; ?>"></option>
+          <?php endforeach; ?>
+        </datalist>
+      </div>
+    </div>
+
+   <div class="control-group">
+      <label class="control-label" for="inputIgnorePrediction">Ignore in predictions</label>
+      <div class="controls">
+        <?php echo Form::checkbox('ignoreprediction',null,false,array('id' => 'inputIgnorePrediction')); ?>
+        &nbsp;&nbsp;<img class="tt" title="Graphs that try to guess your future balance also trigger on transfers, unless you choose to ignore them." src="/img/icons/help.png" alt="Help on this field" />
+        <br /><span class="text-error"><?php echo $errors->first('ignoreprediction'); ?></span>
+        <datalist id="addTransferTarget">
+          <?php foreach($targets as $t): ?>
+          <option value="<?php echo $t; ?>"></option>
+          <?php endforeach; ?>
+        </datalist>
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="inputCountAsExpense">Count as expense</label>
+      <div class="controls">
+        <?php echo Form::checkbox('countasexpense',null,false,array('id' => 'inputCountAsExpense')); ?>
+        &nbsp;&nbsp;<img class="tt" title="If you cannot 'access' the money you transferred, count it as an expense to better reflect this." src="/img/icons/help.png" alt="Help on this field" />
+        <br /><span class="text-error"><?php echo $errors->first('countasexpense'); ?></span>
+        <datalist id="addTransferTarget">
+          <?php foreach($targets as $t): ?>
+          <option value="<?php echo $t; ?>"></option>
+          <?php endforeach; ?>
+        </datalist>
+      </div>
+    </div>
+
+
+
+    <div class="control-group">
+      <div class="controls">
+        <input type="submit" class="btn btn-primary" value="Save new transfer" />
+      </div>
+    </div>
+
+    <?php echo Form::close(); ?>
+
+  </div>
+</div>
+<?php require_once(__DIR__ . '/../layouts/bottom.php') ?>
